768c59838a6b45d5cc0b3f5a7cc00f157bc805e2
english207 <1054765291@qq.com>
2018-12-11 00:52:12 +0800

    添加 模仿bitmap

---
+++ src/main/java/hzf/paxos/bean/AcceptorStatus.java
@@ -0,0 +1,13 @@
+package hzf.paxos.bean;
+
+/**
+ * 决策者记录已处理提案的状态
+ * @author linjx
+ *
+ */
+public enum AcceptorStatus
+{
+	ACCEPTED, //	接受
+	PROMISED, //	承诺 
+	NONE	//	未处理过任何提案
+}
---
+++ src/main/java/hzf/paxos/main/Main.java
@@ -0,0 +1,38 @@
+package hzf.paxos.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import com.zhangq.paxos.doer.Acceptor;
+import com.zhangq.paxos.doer.Proposer;
+
+/**
+ * 主函数 
+ * @author linjx
+ *
+ */
+public class Main
+{
+	private static final int NUM_OF_PROPOSER = 13;
+	private static final int NUM_OF_ACCEPTOR = 7;
+	public static CountDownLatch latch = new CountDownLatch(NUM_OF_PROPOSER);;
+	
+	public static void main(String[] args)
+	{
+		List<Acceptor> acceptors = new ArrayList<>();
+		for	(int i=0;i<NUM_OF_ACCEPTOR;i++)
+		{
+			acceptors.add(new Acceptor());
+		}
+		
+		ExecutorService es = Executors.newCachedThreadPool();
+		for	(int i = 0; i < NUM_OF_PROPOSER; i++)
+		{
+			Proposer proposer =  new Proposer(i, i + "#Proposer", NUM_OF_PROPOSER, acceptors);
+			es.submit(proposer);
+		}
+		es.shutdown();
+	}
+}
